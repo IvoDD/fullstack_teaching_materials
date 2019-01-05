a2d9499ea90aaa9374743f7a342012790eb6a52b
Ivo Dilov <iddilov@gmail.com>
2019-01-05 18:36:19 +0200

    homework 3, js basics 2

---
+++ js-basics2/homework3/width_modifier.js
@@ -0,0 +1,10 @@
+var width = 100;
+
+function smaller(){
+    width = width - 5;
+    document.getElementById("asdf").style.width = width+"px";
+}
+function bigger(){
+    width = width + 5;
+    document.getElementById("asdf").style.width = width+"px";
+}
